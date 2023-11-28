2e5186d1422601fc2061781854df0432b4afdb60
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-28 17:02:33 +0100

    book api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,14 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    if params[:query].present?
+      sql_subquery =<<~SQL
+      books.name ILIKE :query
+      OR books.author ILIKE :query
+      OR books.synopsis ILIKE :query
+      OR books.mood ILIKE :query
+      SQL
+      @books = @books.where(sql_subquery, query: "%#{params[:query]}%")
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @books = Book.all
+    if params[:query].present?
+      @books = @books.where(title: params[:query])
+    end
   end
 end
