1038bcdd2a456830c61d0e134206923f6f49dd9d
mimoun zouhri <zouhrimimoun21@gmail.com>
2023-11-28 16:59:40 +0100

    api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,27 @@
-<<<<<<< HEAD
-# app/controllers/books_controller.rb
-require 'rest-client'
 class BooksController < ApplicationController
-  def fetch_data_from_api
-    url = "https://hapi-books.p.rapidapi.com/nominees/romance/2020"
-    headers = {
-      "X-RapidAPI-Key" => '8637eac372msh15595975c674ed1p18b253jsn459d8714cd9b',
-      "X-RapidAPI-Host" => 'hapi-books.p.rapidapi.com'
-    }
-    begin
-      response = RestClient.get(url, headers: headers)
-      puts response.body
-      @books_data = JSON.parse(response.body)["nominees"]
-    rescue RestClient::ExceptionWithResponse => e
-      puts "Erreur API: #{e.response}"
-      @error_message = "Erreur lors de la récupération des données de l'API. Veuillez vérifier votre clé d'API et les autorisations."
-      @books_data = []
-    end
-
-
+  def index
+    @books = fetch_books
   end
-=======
-class BooksController < ApplicationController
->>>>>>> a4f9cd7c288b542d175dc5819ad14fd5c8e7837d
+
+  private
+
+  def fetch_books
+
+    url = URI("https://hapi-books.p.rapidapi.com/nominees/romance/2020")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = '4ac030e043msh588c37b3b82dc90p1eaf14jsnb60507a44b7c'
+    request["X-RapidAPI-Host"] = 'hapi-books.p.rapidapi.com'
+
+    response = http.request(request)
+
+    if response.code.to_i == 200
+      JSON.parse(response.body)
+    else
+      # Gérer les erreurs ici
+      []
+    end
+  end
 end
