6b96a15b4febb591578c5499b1eb1f5c6d9426ec
Quentin Brice <quentin.brice.qb19@gmail.com>
2023-11-30 12:15:56 +0100

    espoir

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,24 @@ require 'faker'
 require 'open-uri'
 api_key = 'AIzaSyBwshVXgMANcDkVDw-R-mnQ6lmIKljX6gE'
 categories = ['happy', 'sad', 'excited', 'calm', 'serious']
-
 moods = ['happy', 'sad', 'excited', 'calm', 'serious']
-
 categories.each do |category|
   api_url = "https://www.googleapis.com/books/v1/volumes?q=#{category}&key=#{api_key}"
   response = RestClient.get(api_url)
-
   if response.code == 200
     books_data = JSON.parse(response.body)['items']
     puts "Books for #{category}:"
-
     if books_data.present?
-
       books_data.each do |book_data|
         image_thumbnail = book_data['volumeInfo']['imageLinks']&.fetch('thumbnail', nil)
-        authors = book_data['volumeInfo']['authors'] ||
+        authors = book_data['volumeInfo']['authors'] || 'No author'
         Book.create(
           name: book_data['volumeInfo']['title'],
           synopsis: book_data['volumeInfo']['description'] || 'No description available',
-          author: authors.join(', '),
+          author: authors.is_a?(Array) ? authors.join(', ') : 'No author',
           mood: moods.sample,
           category: category,
-          image: image_thumbnail,
+          image: image_thumbnail
         )
       end
     else
@@ -34,5 +29,4 @@ categories.each do |category|
     puts "Error fetching books for #{category}: #{response.code}"
   end
 end
-
 puts 'Seed data created!'
