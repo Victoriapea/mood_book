f3f5fd7e5689c9d522437d4d0ce15cf7b8e8c4eb
Victoria Pea <victoriapea@outlook.fr>
2023-12-06 16:02:49 +0100

    meta2

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,23 +1,21 @@
-# app/helpers/meta_tags_helper.rb
-
 module MetaTagsHelper
-
   def meta_title
-
-  content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
-
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
   end
 
   def meta_description
-
-  content_for?(:meta_description) ? content_for(:meta_description) :                                    DEFAULT_META["meta_description"]
-
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
   end
 
   def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
 
-  meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
-
-  # little twist to make it work equally with an asset or a url
-
-  meta_image.starts_with?("http") ? meta_image : image_url(meta_image) end end
+    # Vérifier si meta_image n'est pas nil avant d'appeler starts_with?
+    if meta_image.present?
+      meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+    else
+      # Retourner une valeur par défaut ou gérer le cas où meta_image est nil
+      DEFAULT_META["default_image"]
+    end
+  end
+end
